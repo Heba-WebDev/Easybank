5ef8352d9938ac38e167feafb2aec71f95ccc563
Heba-Webdev23 <jtvonline@yahoo.com>
2022-04-16 01:40:44 +0300

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+     sm:'480px',
+     md: '768px',
+     lg: '976px',
+     xl: '1440px',
+    },
+    extend: {
+      colors: {
+        darkBlue: 'hsl(233, 26%, 24%)',
+        limeGreen: 'hsl(136, 65%, 51%)',
+        brightCyan: 'hsl(192, 70%, 51%)',
+        grayishBlue: 'hsl(233, 8%, 62%)',
+        lightGrayishBlue: 'hsl(220, 16%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 }
